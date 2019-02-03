6990a8f69d6b10da8b7e24951aaa968cf9937063
znnnnn <846179626@qq.com>
2019-02-04 02:05:12 +0800

    Signin 80%

--- app/page/mine/SignIn.tsx
+++ app/page/mine/SignIn.tsx
@@ -10,13 +10,14 @@ import {
 } from 'react-native'
 import StyleSheet from '@util/stylesheet'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import { Button, InputItem } from '@ant-design/react-native';
+import { Button, InputItem, List } from '@ant-design/react-native';
 import px2dp from '@util/px2dp'
 
 
 export interface State {
-  name: string,
+  phone: string,
   password: string
+  inputBorderColor: string
 }
 
 interface Props {
@@ -26,49 +27,66 @@ interface Props {
 export default class SignIn extends React.Component<Props, State> {
 
   public readonly state: Readonly<State> = {
-    name: '',
-    password: ''
+    phone: '',
+    password: '',
+    inputBorderColor:'#EEEEEE'
   }
 
   constructor(props: {}) {
     super(props);
   }
 
-  // inputItemFocus():void{
-  // }
+  inputItemFocus():void{
+    this.setState({
+      inputBorderColor: '#29A1F7'
+    })
+  }
+
+  inputItemBlur():void{
+    this.setState({
+      inputBorderColor: '#EEEEEE'
+    })
+  }
 
   render() {
     return (
       <View style={styles.root}>
+        <View style={styles.container}>
         <Text style={{ fontSize: 40 }}>微校</Text>
         <View style={styles.inputContainer}>
-          <InputItem
-            clear
-            type="phone"
-            value={this.state.name}
-            onChange={value => {
-              this.setState({
-                name: value,
-              });
-            }}
-            editable={true}
-            disabled={false}
-            placeholder="手机号"
-            // onFocus={this.styles}
-          >
-          </InputItem>
-          <InputItem
-            clear
-            type="password"
-            value={this.state.password}
-            onChange={value => {
-              this.setState({
-                password: value,
-              });
-            }}
-            placeholder="密码"
-          >
-          </InputItem>
+            <InputItem
+              clear
+              type="phone"
+              value={this.state.phone}
+              onChange={value => {
+                this.setState({
+                  phone: value,
+                });
+              }}
+              editable={true}
+              disabled={false}
+              autoFocus={true}
+              placeholder="1111"
+              onFocus={() => this.inputItemFocus()}
+              onBlur={() => this.inputItemBlur()}
+              style={{borderWidth:0}}
+            >
+            </InputItem>
+            <InputItem
+              clear
+              type="password"
+              value={this.state.password}
+              onChange={value => {
+                this.setState({
+                  password: value,
+                });
+              }}
+              placeholder="密码"
+              onFocus={() => this.inputItemFocus()}
+              onBlur={() => this.inputItemBlur()}
+              style={{borderBottomColor: this.state.inputBorderColor}}
+            >
+            </InputItem>
         </View>
         <Button type="primary" style={styles.loginBtn}>登录</Button>
         <View style={styles.actions}>
@@ -80,14 +98,15 @@ export default class SignIn extends React.Component<Props, State> {
             <Text>注册</Text>
           </TouchableOpacity>
         </View>
+        </View>
         <View style={styles.socialContainer}>
           <View style={styles.social}>
             <Text>QQ</Text>
             <Text>微信</Text>
             <Text>微博</Text>
           </View>
-        </View>
         <Text style={{ justifyContent: 'center', fontSize: 16, color: '#333' }}>我是游客</Text>
+        </View>
       </View>
     );
   }
@@ -96,20 +115,21 @@ export default class SignIn extends React.Component<Props, State> {
 const styles = StyleSheet.create({
   root: {
     alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: '#fff',
+    height: hp('100%'),
   },
   container: {
-    marginTop: hp('10%'),
-    alignItems: 'center',
-    width: wp('90%')
+    // marginTop: hp('10%'),
+    // alignItems: 'center',
+    // width: wp('90%')
   },
   inputContainer: {
     height: 140,
     width: wp('90%'),
     justifyContent: 'space-around',
-    borderWidth: 1,
-    borderColor: 'blue',
-    marginTop: 20
+    // borderWidth: 1,
+    // borderColor: 'blue',
+    marginTop: 10
   },
   loginBtn: {
     borderRadius: 20,
@@ -126,8 +146,9 @@ const styles = StyleSheet.create({
     fontSize: 10
   },
   socialContainer: {
-    paddingTop: 200,
-    backgroundColor: '#eee'
+    backgroundColor: '#eee',
+    position: 'absolute',
+    bottom: 0
   },
   social: {
     width: wp('80%'),
