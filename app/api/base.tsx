2c9481d08819633202ba7601732ee2e13382b12c
znnnnn <846179626@qq.com>
2019-07-04 09:07:55 +0800

    update: 服务端更换服务器

--- app/api/base.tsx
+++ app/api/base.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Description: 
+ * @version: 
+ * @Author: znnnnn
+ * @Date: 2019-07-04 08:44:51
+ * @LastEditors: znnnnn
+ * @LastEditTime: 2019-07-04 09:07:32
+ */
 /**
  * 接口域名的管理
  */
@@ -6,9 +14,9 @@
 
 const base = {
   // sq: '/api', // 在线地址api -> '/api'在'webpack.config.client.js中映射为'http://101.132.141.130:82/api/'
-  sq: 'http://111.231.116.130:8080', // 在线地址api -> '/api'在'webpack.config.client.js中映射为'http://101.132.141.130:82/api/'
+  sq: 'http://139.224.134.32:8080', // 在线地址api -> '/api'在'webpack.config.client.js中映射为'http://101.132.141.130:82/api/'
   // bd: 'http://172.20.10.3:8080'  // '本地开发api  数据mock模拟'
-  bd: 'http://111.231.116.130:8080' // 腾讯云
+  bd: 'http://139.224.134.32:8080' // 腾讯云
   // token: store.state.token
 }
 
--- wx/main.js
+++ wx/main.js
@@ -1,33 +1,41 @@
+/*
+ * @Description: 
+ * @version: 
+ * @Author: znnnnn
+ * @Date: 2019-07-04 08:44:51
+ * @LastEditors: znnnnn
+ * @LastEditTime: 2019-07-04 09:02:28
+ */
 new Vue({
-  el: "#app",
+  el: '#app',
   data: {
     tableData: []
   },
   mounted() {
-    axios.get("http://111.231.116.130:8080/user/code").then(res => {
-      phone = Object.keys(res.data);
-      /**
-       * 遍历数据，返回结构数据
-       */
-      let data = phone.map(item => {
-        return {
-          date: new Date(res.data[item]["date"]).toLocaleString(),
-          phone: res.data[item]["phone"],
-          code: res.data[item]["code"],
-          timeStamp: new Date(res.data[item]["date"])
-        };
-      });
+    setInterval(() => {
+      axios.get('http://111.231.116.130:8080/user/code').then((res) => {
+        phone = Object.keys(res.data)
+        /**
+         * 遍历数据，返回结构数据
+         */
+        let data = phone.map((item) => {
+          return {
+            date: new Date(res.data[item]['date']).toLocaleString(),
+            phone: res.data[item]['phone'],
+            code: res.data[item]['code'],
+            timeStamp: new Date(res.data[item]['date'])
+          }
+        })
 
-      /**
-       * 过滤五分钟内的验证码
-       */
-      this.tableData = data.filter(
-        item=>{
-          var codeDate = item["timeStamp"]
-          let now = new Date();
-          return ((now-codeDate) / 1000) < 300
-        }
-      )
-    });
+        /**
+         * 过滤五分钟内的验证码
+         */
+        this.tableData = data.filter((item) => {
+          var codeDate = item['timeStamp']
+          let now = new Date()
+          return (now - codeDate) / 1000 < 300
+        })
+      })
+    }, 500)
   }
-});
+})
